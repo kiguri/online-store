84a9e5c3d9b7199f19f92c9c9edea9e2aacb2d97
HuyHQ <huyha14082012@gmail.com>
2020-12-15 10:31:59 +0700

    add route which send back paypal_id

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
+app.get('/api/config/paypal', (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 //middleware
 app.use(notFound);
 
