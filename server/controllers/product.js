4d231eb9d952c500052fff1e9c34ccccb570f331
HuyHQ <huyha14082012@gmail.com>
2020-12-16 15:48:40 +0700

    create product, update product endpoint

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -49,8 +49,73 @@ const deleteProduct = async (req, res) => {
     }
 };
 
+// POST /api/products
+// Create a product
+const createProduct = async (req, res) => {
+    try {
+        const product = new Product({
+            name: 'Escape Run',
+            price: 80,
+            user: req.user._id,
+            image:
+                'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5a591d6c-1a30-49f1-8413-2fdc9e04b9e2/react-escape-run-running-shoe-94nDwX.jpg',
+            brand: 'Nike',
+            category: 'Women',
+            countInStock: 0,
+            numReviews: 0,
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        });
+
+        const createdProduct = await product.save();
+        res.status(201).json(createdProduct);
+    } catch (error) {
+        const statusCode = res.statusCode !== 500 ? res.statusCode : 500;
+        res.status(statusCode).send({ message: error.message });
+    }
+};
+
+// PUT /api/products/:id
+// Update a product
+const updateProduct = async (req, res) => {
+    try {
+        const {
+            name,
+            price,
+            description,
+            image,
+            brand,
+            category,
+            countInStock,
+        } = req.body;
+
+        const product = await Product.findById(req.params.id);
+
+        if (product) {
+            product.name = name;
+            product.price = price;
+            product.description = description;
+            product.image = image;
+            product.brand = brand;
+            product.category = category;
+            product.countInStock = countInStock;
+
+            const updatedProduct = await product.save();
+            res.json(updatedProduct);
+        } else {
+            res.status(404);
+            throw new Error('Product not found');
+        }
+    } catch (error) {
+        const statusCode = res.statusCode !== 500 ? res.statusCode : 500;
+        res.status(statusCode).send({ message: error.message });
+    }
+};
+
 module.exports = {
     fetchAllProduct,
     fetchProductById,
     deleteProduct,
+    createProduct,
+    updateProduct,
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,15 +3,21 @@ const {
     fetchAllProduct,
     fetchProductById,
     deleteProduct,
+    createProduct,
+    updateProduct,
 } = require('../controllers/product');
 const { protect, admin } = require('../middleware/auth');
 const productRouter = express.Router();
 
-productRouter.route('/').get(fetchAllProduct);
+productRouter
+    .route('/')
+    .get(fetchAllProduct)
+    .post(protect, admin, createProduct);
 
 productRouter
     .route('/:id')
     .get(fetchProductById)
-    .delete(protect, admin, deleteProduct);
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct);
 
 module.exports = productRouter;
